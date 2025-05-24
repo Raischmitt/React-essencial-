acdb11137e747c1278c8a8e5ff05feaeae4f854e
Raissa Schmitt <raissajoaquim29@gmail.com>
2025-05-23 22:05:41 -0300

    utilizando o useStatee atualizando data.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { CORE_CONCEPTS } from './data';
+import { useState } from 'react';
+import { CORE_CONCEPTS, EXAMPLES } from './data';
 import Header from "./Components/Header.jsx";
 import CoreConcept from './Components/CoreConcept.jsx';
 import TabButton from './Components/TabButton.jsx';
 
 
+
 function App() {
-  function handleSelect() {
-    console.log(selectdButton)
+  const [ selectedTopic, setSelectedTopic] = useState('components')
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
   }
 
   return (
@@ -27,10 +31,18 @@ function App() {
           <menu>
             <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
             <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('prps')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
 
       </main>
